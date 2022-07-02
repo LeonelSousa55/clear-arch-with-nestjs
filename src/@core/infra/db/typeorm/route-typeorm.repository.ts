882416d0ecbd8ccccd7cf3f2786fe0fc9c4a155a
Leonel <leonel55sousa@gmail.com>
2022-07-02 14:54:10 -0300

    feat: first commit

---
+++ src/@core/infra/db/typeorm/route-typeorm.repository.ts
@@ -0,0 +1,16 @@
+import { Route } from "src/@core/domain/route.entity";
+import { RouteRepositoryInterface } from "../../../domain/route.repository";
+import { Repository } from 'typeorm';
+
+export class RouteTypeOrmRepository implements RouteRepositoryInterface {
+
+    constructor(private ormRepo: Repository<Route>) { }
+
+    async insert(route: Route): Promise<void> {
+        await this.ormRepo.save(route);
+    }
+    
+    findAll(): Promise<Route[]> {
+        return this.ormRepo.find();
+    }
+}
